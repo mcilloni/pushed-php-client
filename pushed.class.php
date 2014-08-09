0312c042e5640541cb19024ef9981813472775d9
Marco Cilloni <marco.cilloni@yahoo.com>
2014-08-09 11:01:57 +0200

    Licence header added

--- pushed.class.php
+++ pushed.class.php
@@ -1,4 +1,17 @@
 <?php
+
+#  pushed-php-client is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software 
+#  Foundation, either version 3 of the License, or (at your option) any later version. 
+#
+#  pushed-php-client is distributed in the hope that it will be useful, but 
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pushed-php-client.  If not, see <http://www.gnu.org/licenses/>
+#
+#  Copyright (C) Marco Cilloni <marco.cilloni@yahoo.com> 2014
+
 namespace MCilloni/Pushed;
 
 class Pushed {
